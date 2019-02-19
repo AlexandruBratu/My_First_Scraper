fb784e88c32bc501d642d38f00067b5c17b72320
AlexandruBratu <43742145+AlexandruBratu@users.noreply.github.com>
2019-02-19 12:33:20 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,6 +10,7 @@ html = scraperwiki.scrape("http://foo.com")
 # This line scrapes the html of this web page
 print(html)
 # This line prints the html content of the web page for us to see
+record = {}
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
 root.cssselect("div#footer")
@@ -20,8 +21,11 @@ listofmatches = root.cssselect("a")
 for match in listofmatches:
   print(match)
   print(lxml.html.tostring(match))
+  record["link"] = lxml.html.tostring(match)
+  print(record)
+  scraperwiki.sqlite.save(unique_keys=["link"], data=record)
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
